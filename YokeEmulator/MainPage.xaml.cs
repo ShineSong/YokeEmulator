55ddbd6e4e8acfae7e549eabffd5f79a3a7d2c3f
ShineSong <xcssss@sina.com>
2015-08-13 22:26:46 +0800

    initial commit

---
+++ YokeEmulator/MainPage.xaml.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Streams;
+using Windows.Devices.Sensors;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+
+
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=391641 上有介绍
+
+namespace YokeEmulator
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Accelerometer accelerometer = null;
+        bool connected = false;
+
+        StreamSocket axisSocket = null;
+        const int AxisMsgSize = 18;
+
+        StreamSocket ctlSocket = null;
+        const int CtlMsgSize = 11;
+
+        bool povTouched = false;
+        Ellipse povPoint = null;
+        const int povPointSize= 20;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            Ellipse bgEllipse = new Ellipse();
+            bgEllipse.Width = povCtl.Width;
+            bgEllipse.Height = povCtl.Height;
+            bgEllipse.Fill = new SolidColorBrush(Colors.SkyBlue);
+            povCtl.Children.Add(bgEllipse);
+
+            povPoint = new Ellipse();
+            var margin = povPoint.Margin;
+            margin.Left = povCtl.Width / 2- povPointSize/2;
+            margin.Top = povCtl.Height / 2- povPointSize/2;
+            povPoint.Margin = margin;
+            povPoint.Width = povPointSize;
+            povPoint.Height = povPointSize;
+            povPoint.Fill = new SolidColorBrush(Colors.Red);
+            povPoint.Stroke = new SolidColorBrush(Colors.Yellow);
+            povCtl.Children.Add(povPoint);
+        }
+
+        /// <summary>
+        /// 在此页将要在 Frame 中显示时进行调用。
+        /// </summary>
+        /// <param name="e">描述如何访问此页的事件数据。
+        /// 此参数通常用于配置页。</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: 准备此处显示的页面。
+
+            // TODO: 如果您的应用程序包含多个页面，请确保
+            // 通过注册以下事件来处理硬件“后退”按钮:
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed 事件。
+            // 如果使用由某些模板提供的 NavigationHelper，
+            // 则系统会为您处理该事件。
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Settings));
+        }
+
+        private void CalibrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private async void connectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (connected)
+            {
+                //disconnecting
+                try
+                {
+                    axisSocket.Dispose();
+                    ctlSocket.Dispose();
+                    connected = false;
+                    connectBtn.Foreground = new SolidColorBrush(Colors.Green);
+                    connectBtn.Content = connected ? "DisConnect" : "Connect";
+                }
+                catch (Exception)
+                {
+                    connectBtn.Foreground = new SolidColorBrush(Colors.Red);
+                    connectBtn.Content = "FAILED";
+                }
+            }
+            else
+            {
+                try
+                {
+                    //connecting
+                    var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+                    axisSocket = new StreamSocket();
+                    ctlSocket = new StreamSocket();
+                    HostName hostname = new HostName(localSettings.Values["IPADDR"].ToString());
+                    await axisSocket.ConnectAsync(hostname, "23333");
+                    await ctlSocket.ConnectAsync(hostname, "23334");
+
+                    connected = true;
+                    connectBtn.Foreground = new SolidColorBrush(Colors.Green);
+                    connectBtn.Content = connected ? "DisConnect" : "Connect";
+                }
+                catch (Exception exception)
+                {
+                    switch (SocketError.GetStatus(exception.HResult))
+                    {
+                        case SocketErrorStatus.HostNotFound:
+                            // Handle HostNotFound Error
+                            connectBtn.Foreground = new SolidColorBrush(Colors.Red);
+                            connectBtn.Content = "HostNotFound";
+                            break;
+                        default:
+                            // Handle Unknown Error
+                            connectBtn.Foreground = new SolidColorBrush(Colors.Red);
+                            connectBtn.Content = "FAILED";
+                            break;
+                    }
+                    axisSocket.Dispose();
+                    ctlSocket.Dispose();
+                }
+            }
+        }
+
+        private void accelerometerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // If the accelerometer is null, it is initialized and started
+            if (accelerometer == null)
+            {
+                // Instantiate the accelerometer sensor object
+                accelerometer = Accelerometer.GetDefault();
+
+                // Add an event handler for the ReadingChanged event.
+                accelerometer.ReadingChanged += new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(accelerometer_ReadingChanged);
+
+                // The Start method could throw and exception, so use a try block
+                accelerometerBtn.Background = new SolidColorBrush(Colors.Green);
+                accelerometerBtn.Content = "OFF";
+            }
+            else
+            {
+                // if the accelerometer is not null, call Stop
+                accelerometer = null;
+                accelerometerBtn.Background = new SolidColorBrush(Colors.Red);
+                accelerometerBtn.Content = "ON";
+            }
+        }
+
+        async void accelerometer_ReadingChanged(object sender, AccelerometerReadingChangedEventArgs e)
+        {
+            if (accelerometer != null)
+            {
+                if (connected)
+                {
+                    //byte[] bx = BitConverter.GetBytes(e.Reading.AccelerationX);
+                    byte[] by = BitConverter.GetBytes(-e.Reading.AccelerationY * 0.5 + 0.5);
+                    byte[] bz = BitConverter.GetBytes(e.Reading.AccelerationZ + 1);
+
+                    byte[] buff = new byte[AxisMsgSize];
+                    by.CopyTo(buff, 1);
+                    bz.CopyTo(buff, 9);
+                    buff[0] = 255;
+                    buff[AxisMsgSize-1] = 0;
+                    await axisSocket.OutputStream.WriteAsync(buff.AsBuffer());
+                }
+            }
+        }
+
+        private async void throttleSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] param = BitConverter.GetBytes(e.NewValue/100.0);   
+
+                byte[] buff = new byte[CtlMsgSize];
+                buff[1] = (byte)'t';
+                param.CopyTo(buff, 2);
+                buff[0] = 255;
+                buff[CtlMsgSize-1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn1_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 1;
+                buff[3] = 1;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn1_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 1;
+                buff[3] = 0;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn2_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 2;
+                buff[3] = 1;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn2_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 2;
+                buff[3] = 0;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn3_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 3;
+                buff[3] = 1;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn3_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 3;
+                buff[3] = 0;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn4_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 4;
+                buff[3] = 1;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void Btn4_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                buff[0] = 255;
+                buff[1] = (byte)'b';
+                buff[2] = 4;
+                buff[3] = 0;
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private void povCtl_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            povTouched = true;
+        }
+
+        private async void povCtl_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            povTouched = false;
+            var margin = povPoint.Margin;
+            margin.Left = povCtl.Width / 2 - povPointSize / 2;
+            margin.Top = povCtl.Height / 2 - povPointSize / 2;
+            povPoint.Margin = margin;
+            if (connected)
+            {
+                byte[] buff = new byte[CtlMsgSize];
+                byte[] angle = BitConverter.GetBytes((double)-1);
+                buff[0] = 255;
+                buff[1] = (byte)'p';
+                angle.CopyTo(buff, 2);
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+
+        private async void povCtl_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (povTouched && connected)
+            {
+                var _curpoint = e.GetCurrentPoint(povCtl);
+                var margin = povPoint.Margin;
+                margin.Left = _curpoint.Position.X - povPointSize / 2;
+                margin.Top = _curpoint.Position.Y - povPointSize / 2;
+                povPoint.Margin = margin;
+
+                double dx = _curpoint.Position.X - povCtl.Width / 2;
+                double dy = _curpoint.Position.Y - povCtl.Height / 2;
+                double angle = 180 - Math.Atan2(dx, dy) * 180 / Math.PI;
+
+                byte[] buff = new byte[CtlMsgSize];
+                byte[] angleByte = BitConverter.GetBytes(angle);
+                buff[0] = 255;
+                buff[1] = (byte)'p';
+                angleByte.CopyTo(buff, 2);
+                buff[CtlMsgSize - 1] = 0;
+                await ctlSocket.OutputStream.WriteAsync(buff.AsBuffer());
+            }
+        }
+    }
+}
---
+++ YokeEmulatorServer/Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using vJoyInterfaceWrap;
+namespace AcceleratorMeterServer
+{
+    class Program
+    {
+        static public vJoy joystick;
+        static public vJoy.JoystickState iReport;
+        static public uint id = 1;
+
+        static long axisMaxval;
+        static byte[] axisBuffer = new byte[AxisMsgSize];
+        const int AxisPort = 23333;
+        const int AxisMsgSize = 18;
+
+        static long sliderMaxval;
+        static byte[] ctlBuffer = new byte[CtlMsgSize];
+        const int CtlPort = 23334;
+        const int CtlMsgSize = 11;
+
+        static void Main(string[] args)
+        {
+            joystick = new vJoy();
+            iReport = new vJoy.JoystickState();
+            if (!joystick.vJoyEnabled())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("vJoy driver not enabled: Failed Getting vJoy attributes.\n");
+                return;
+            }
+            else
+                Console.WriteLine("Vendor: {0}\nProduct :{1}\nVersion Number:{2}\n", joystick.GetvJoyManufacturerString(), joystick.GetvJoyProductString(), joystick.GetvJoySerialNumberString());
+            // Get the state of the requested device
+            VjdStat status = joystick.GetVJDStatus(id);
+            switch (status)
+            {
+                case VjdStat.VJD_STAT_OWN:
+                    Console.WriteLine("vJoy Device {0} is already owned by this feeder\n", id);
+                    break;
+                case VjdStat.VJD_STAT_FREE:
+                    Console.WriteLine("vJoy Device {0} is free\n", id);
+                    break;
+                case VjdStat.VJD_STAT_BUSY:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("vJoy Device {0} is already owned by another feeder\nCannot continue\n", id);
+                    return;
+                case VjdStat.VJD_STAT_MISS:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("vJoy Device {0} is not installed or disabled\nCannot continue\n", id);
+                    return;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("vJoy Device {0} general error\nCannot continue\n", id);
+                    return;
+            };
+            // Check which axes are supported
+            bool AxisX = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_X);
+            bool AxisY = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_Y);
+            bool AxisZ = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_Z);
+            bool AxisRX = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_RX);
+            bool AxisRZ = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_RZ);
+            // Get the number of buttons and POV Hat switchessupported by this vJoy device
+            int nButtons = joystick.GetVJDButtonNumber(id);
+            int ContPovNumber = joystick.GetVJDContPovNumber(id);
+            int DiscPovNumber = joystick.GetVJDDiscPovNumber(id);
+
+            // Print results
+            Console.WriteLine("\nvJoy Device {0} capabilities:\n", id);
+            Console.WriteLine("Numner of buttons\t\t{0}\n", nButtons);
+            Console.WriteLine("Numner of Continuous POVs\t{0}\n", ContPovNumber);
+            Console.WriteLine("Numner of Descrete POVs\t\t{0}\n", DiscPovNumber);
+            Console.WriteLine("Axis X\t\t{0}\n", AxisX ? "Yes" : "No");
+            Console.WriteLine("Axis Y\t\t{0}\n", AxisX ? "Yes" : "No");
+            Console.WriteLine("Axis Z\t\t{0}\n", AxisX ? "Yes" : "No");
+            Console.WriteLine("Axis Rx\t\t{0}\n", AxisRX ? "Yes" : "No");
+            Console.WriteLine("Axis Rz\t\t{0}\n", AxisRZ ? "Yes" : "No");
+
+            // Test if DLL matches the driver
+            UInt32 DllVer = 0, DrvVer = 0;
+            bool match = joystick.DriverMatch(ref DllVer, ref DrvVer);
+            if (match)
+                Console.WriteLine("Version of Driver Matches DLL Version ({0:X})\n", DllVer);
+            else
+                Console.WriteLine("Version of Driver ({0:X}) does NOT match DLL Version ({1:X})\n", DrvVer, DllVer);
+
+            // Acquire the target
+            if ((status == VjdStat.VJD_STAT_OWN) || ((status == VjdStat.VJD_STAT_FREE) && (!joystick.AcquireVJD(id))))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Failed to acquire vJoy device number {0}.\n", id);
+                return;
+            }
+            else
+                Console.WriteLine("Acquired: vJoy device number {0}.\n", id);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("ALL GREEN.");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            joystick.GetVJDAxisMax(id, HID_USAGES.HID_USAGE_X, ref axisMaxval);
+
+
+            try
+            {
+                TcpListener axisTcp;
+                axisTcp = new TcpListener(IPAddress.Any, AxisPort);
+                axisTcp.Start(1);
+                axisTcp.BeginAcceptSocket(axisClientConnect, axisTcp);
+                
+                TcpListener ctlTcp;
+                ctlTcp = new TcpListener(IPAddress.Any, CtlPort);
+                ctlTcp.Start(1);
+                ctlTcp.BeginAcceptSocket(ctlClientConnect, ctlTcp);
+                Console.WriteLine("started listening...");
+
+            }
+            catch (System.Security.SecurityException)
+            {
+                Console.WriteLine("firewall banned application.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            while (true) Thread.Sleep(1000000);
+        }
+        
+        static void axisClientConnect(IAsyncResult ar)
+        {
+            TcpListener ServerSocket = (TcpListener)ar.AsyncState;
+            Socket ClientSocket = ServerSocket.EndAcceptSocket(ar);
+            Console.WriteLine("Axis Channel Client Connected.");
+            joystick.ResetVJD(id);
+
+            ClientSocket.BeginReceive(axisBuffer, 0, AxisMsgSize, SocketFlags.None, new AsyncCallback(axisRead), ClientSocket);
+            ServerSocket.BeginAcceptSocket(new AsyncCallback(axisClientConnect), ServerSocket);
+        }
+
+        static void ctlClientConnect(IAsyncResult ar)
+        {
+            TcpListener ServerSocket = (TcpListener)ar.AsyncState;
+            Socket ClientSocket = ServerSocket.EndAcceptSocket(ar);
+            Console.WriteLine("Ctrl Channel Client Connected.");
+            joystick.ResetVJD(id);
+
+            ClientSocket.BeginReceive(ctlBuffer, 0, CtlMsgSize, SocketFlags.None, new AsyncCallback(ctlRead), ClientSocket);
+            ServerSocket.BeginAcceptSocket(new AsyncCallback(ctlClientConnect), ServerSocket);
+        }
+
+        static void axisRead(IAsyncResult ar)
+        {
+            Socket SocketClient = (Socket)ar.AsyncState;
+            int ByteRead = 0;
+            try
+            {
+                ByteRead = SocketClient.EndReceive(ar);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Axis Channel Disconnect.");
+                return;
+            }
+            if (ByteRead ==0)
+            {
+                Console.WriteLine("Axis Channel Disconnect.");
+                return;
+            }
+            else if(ByteRead <AxisMsgSize)
+            {
+                Console.WriteLine("Axis Channel Error Connect.");
+                Console.Beep();
+                return;
+            }
+
+            byte veri1 = axisBuffer[0];
+            byte veri2 = axisBuffer[AxisMsgSize - 1];
+
+            if (veri1 != 255 || veri2 != 0)
+            {
+                Console.WriteLine("Axis Channel Verify Error.");
+                Console.Beep();
+                return;
+            }
+
+            double x = BitConverter.ToDouble(axisBuffer, 1) * axisMaxval;
+            double y = BitConverter.ToDouble(axisBuffer, 9) * axisMaxval;
+            if (x < 0) x = 0; else if (x > axisMaxval) x = axisMaxval;
+            if (y < 0) y = 0; else if (y > axisMaxval) y = axisMaxval;
+            joystick.SetAxis((int)x, id, HID_USAGES.HID_USAGE_X);
+            joystick.SetAxis((int)y, id, HID_USAGES.HID_USAGE_Y);
+
+            SocketClient.BeginReceive(axisBuffer, 0, AxisMsgSize, SocketFlags.None, new AsyncCallback(axisRead), SocketClient);
+        }
+
+        static void ctlRead(IAsyncResult ar)
+        {
+            Socket SocketClient = (Socket)ar.AsyncState;
+            int ByteRead = 0;
+            try
+            {
+                ByteRead = SocketClient.EndReceive(ar);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ctrl Channel Disconnect.");
+                return;
+            }
+            if (ByteRead == 0)
+            {
+                Console.WriteLine("Ctrl Channel Disconnect.");
+                return;
+            }
+            else if (ByteRead < CtlMsgSize)
+            {
+                Console.WriteLine("Ctrl Channel Error Connect.");
+                Console.Beep();
+                return;
+            }
+
+            byte veri1 = ctlBuffer[0];
+            byte veri2 = ctlBuffer[CtlMsgSize - 1];
+
+            if (veri1 != 255 || veri2 != 0)
+            {
+                Console.WriteLine("Ctrl Channel Verify Error.");
+                Console.Beep();
+                return;
+            }
+            byte command = ctlBuffer[1];
+            switch (command)
+            {
+                case (byte)'t':
+                    double param = BitConverter.ToDouble(ctlBuffer, 2);
+                    joystick.SetAxis((int)(param * axisMaxval), id, HID_USAGES.HID_USAGE_SL0);
+                    break;
+                case (byte)'b':
+                    byte bid = ctlBuffer[2];
+                    byte state = ctlBuffer[3];
+                    joystick.SetBtn(state == 1, id, bid);
+                    break;
+                case (byte)'p':
+                    double pov = BitConverter.ToDouble(ctlBuffer, 2);
+                    if(pov>0)
+                        joystick.SetContPov((int)pov*100, id, 1);
+                    else
+                        joystick.SetContPov(-1, id, 1);
+                    break;
+                default:
+                    Console.WriteLine("Ctrl command unknown.");
+                    break;
+            }
+
+            SocketClient.BeginReceive(ctlBuffer, 0, CtlMsgSize, SocketFlags.None, new AsyncCallback(ctlRead), SocketClient);
+        }
+    }
+
+}
+
